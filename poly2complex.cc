f3e74cb7585184005083e0e49dd8f9c202f169f2
scaldov <scaldov@scyld.sky>
2018-12-06 00:15:19 +0400

    Added complex solution

---
+++ poly2complex.cc
@@ -0,0 +1,41 @@
+/*
+ * compile with GNU compiler collection 3.6.+:
+ * g++ -o poly2complex poly2complex.cc
+ */
+
+#include <string>
+#include <iostream>
+#include <math.h>
+#include <complex>
+
+//use c++14 lambda with capture by value
+
+int main(int argc, char *argv[]) {
+    // print warning and usage help if not enough arguments given
+    if(argc < 4) {
+        std::cout << "run " << argv[0] << " with 3 line options: a, b and c" << std::endl;
+        std::cout << "e.g." << std::endl;
+        std::cout << argv[0] << " 1 -5 6" << std::endl;
+        return -1;
+    }
+    double a, b, c;
+    // complex field will be used to solve eq.
+    std::complex<double> discriminant, root_discriminant, x1, x2;
+    // convert lineopts string arguments to doubles
+    a = std::stod(argv[1]);
+    b = std::stod(argv[2]);
+    c = std::stod(argv[3]);
+    // find discriminant
+    discriminant = b * b - 4 * a * c;
+    std::cout << "equation  " << std::endl << a << "x^2 + " << b << "x + " << c << " = 0" << std::endl;
+    if(discriminant == 0.0) { // 2-multiple root
+        x1 = -b / (2.0 * a);
+        std::cout << "has 2-multiple root x = " << x1 << std::endl;
+    } else {
+        root_discriminant = sqrt(discriminant);
+        x1 = (-b + root_discriminant) / (2.0 * a);
+        x2 = (-b - root_discriminant) / (2.0 * a);
+        std::cout << "has roots x1 = " << x1 << " and x2 = " << x2 << std::endl;
+    }
+    return 0;
+}
